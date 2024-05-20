82dc39c121bbfb94868c7d5eb4c495f7d78aca61
sungjin <sungjine456@gmail.com>
2024-05-20 16:24:16 +0900

    유효성검사 추가

--- frontend/src/views/StockAddView.vue
+++ frontend/src/views/StockAddView.vue
@@ -7,11 +7,38 @@ import { ref } from "vue";
 
 const stock = ref<Stock>({ name: "", code: "", stockKind: StockKind.ETF });
 
-const addStock = () => {
-  if (stock.value.name === "" || stock.value.code === "") {
-    // TODO 저장되지 않는 이유 사용자에게 안내하기
-    return;
+const errors = ref<Map<string, string>>(new Map<string, string>());
+
+const validaters = () => {
+  const name = validater("name");
+  const code = validater("code");
+
+  return name && code;
+};
+
+const validater = (type: string) => {
+  let value = "";
+  let msg = "";
+
+  if (type === "name") {
+    value = stock.value.name;
+    msg = "이름이 없습니다.";
+  } else if (type === "code") {
+    value = stock.value.code;
+    msg = "코드가 없습니다.";
   }
+
+  if (value === "") {
+    errors.value.set(type, msg);
+    return false;
+  } else {
+    errors.value.delete(type);
+    return true;
+  }
+};
+
+const addStock = () => {
+  if (!validaters()) return;
 
   axios
     .post("/api/stock", stock.value)
@@ -38,12 +65,49 @@ const addStock = () => {
     </select>
   </div>
   <div>
-    <label>이름</label>
-    <input v-model="stock.name" />
+    <label for="name">이름</label>
+    <input id="name" v-model="stock.name" @keyup="validater('name')" />
+  </div>
+  <div class="error-box">
+    <span class="error" v-show="errors.has('name')">{{ errors.get("name") }}</span>
   </div>
   <div>
-    <label>코드</label>
-    <input v-model="stock.code" />
+    <label for="code">코드</label>
+    <input id="code" v-model="stock.code" @keyup="validater('code')" />
   </div>
-  <button @click="addStock">추가</button>
+  <div class="error-box">
+    <span class="error" v-show="errors.has('code')">{{ errors.get("code") }}</span>
+  </div>
+  <div>
+    <button @click="addStock">추가</button>
+  </div>
 </template>
+
+<style scoped>
+label {
+  margin-right: 5px;
+}
+
+button {
+  float: right;
+  margin-top: 5px;
+}
+
+input,
+select {
+  width: 180px;
+}
+
+.error-box {
+  text-align: end;
+  height: 10px;
+  font-size: 10px;
+  line-height: 1;
+  margin-top: 2px;
+}
+
+.error {
+  color: red;
+  margin-left: 5px;
+}
+</style>
